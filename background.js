37fd28c269391d3b613c8b60118836fcbdde7b93
Y.Sasaki <y-sasaki@intworks.co.jp>
2019-07-23 00:36:57 +0900

    [upd]原文(reload+scrollTop)と翻訳文(google翻訳バー削除)

---
+++ background.js
@@ -0,0 +1,50 @@
+chrome.contextMenus.create({
+  "title" : "原文と翻訳文を比較する",
+  "type" : "normal",
+//   "onclick" : excecute(),
+});
+chrome.browserAction.onClicked.addListener(function(tab){
+	excecute(tab);
+});
+
+function excecute(tab){
+	let windowWidth = 0;
+	let windowHeight = 0;
+	chrome.tabs.reload(tab.id);
+	chrome.windows.getCurrent(function(w) {
+		windowWidth = screen.availWidth; //タスクバーなどを除くモニタ有効域の幅
+		windowHeight = screen.availHeight; //タスクバーなどを除くモニタ有効域の高さ
+
+		chrome.windows.update(w.id,
+							{
+								left: screen.availLeft,
+								top: screen.availTop,
+								width: windowWidth / 2, 
+								height: windowHeight
+							}, 
+							() => {
+									chrome.tabs.executeScript(tab.id,
+										{
+											code: 'window.scrollTo(0, 0);'
+										}
+									);});
+							
+		chrome.windows.create({
+								url: "http://translate.google.com/translate?u=" + tab.url, 
+								left: windowWidth / 2 + screen.availLeft,
+								top: screen.availTop,
+								width: windowWidth / 2, 
+								height: windowHeight
+							}, 
+							tab => {
+									chrome.tabs.executeScript(tab.id,
+										{
+											code: `searchBar = document.getElementById('wtgbr');
+												   translationFromTo = document.getElementById('gt-c');
+												   body.removeChild(searchBar);`
+										}
+									)
+							});
+	});
+}
+
